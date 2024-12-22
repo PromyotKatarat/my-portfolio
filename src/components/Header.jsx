3a7744110d96674dbeb9e3ff58e8ab1c3f000924
Promyot Katarat <promyot.k1@gmail.com>
2024-12-22 20:47:59 +0700

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => (
   <header className="bg-gray-900 text-white p-4">
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold">Promyot Katarat</h1>
-      <p className="mt-2">TechBro - Developer</p>
+      <p className="mt-2">Brogrammer and extrovert It support</p>
     </div>
   </header>
 );
